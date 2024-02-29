cab5a7ee686591653e6f6a3be01f1ecdd56bcb91
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2024-02-28 21:50:00 -0600

    Agregar archivos de proyecto.

---
+++ Sol_Minimarket.Datos/Conexion.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Sol_Minimarket.Datos
+{
+    public class Conexion
+    {
+        private string Base;
+        private string Servidor;
+        private string Usuario;
+        private string Clave;
+        private bool Seguridad;
+        private static Conexion Con = null;
+
+        private Conexion()
+        {
+            this.Base = "BD_MINIMARKET";
+            this.Servidor = "DESKTOP-GKONGVJ\\SQLEXPRESS";
+            this.Usuario = "sistemas";
+            this.Clave = "soporte";
+            this.Seguridad = false;
+        }
+
+        public SqlConnection CrearConexion()
+        {
+            SqlConnection Cadena = new SqlConnection();
+            try
+            {
+                // Definir si la conexion se realiza con un usuario y password, o con autenticación de Windows
+                Cadena.ConnectionString = "Server=" + this.Servidor + "; Database=" + this.Base + ";";
+                if (Seguridad)
+                {
+                    Cadena.ConnectionString = Cadena.ConnectionString + "Integrated Security= SSPI";
+                }
+                else
+                {
+                    Cadena.ConnectionString = Cadena.ConnectionString +"User Id="+this.Usuario+"; Password="+this.Clave;
+                }
+            }
+            catch (Exception ex)
+            {
+                Cadena = null;
+                throw ex;
+            }
+            return Cadena;
+        }
+
+        public static Conexion getInstancia()
+        {
+            if (Con == null)
+            {
+                Con = new Conexion();
+            }
+            return Con;
+        }
+    }
+}
