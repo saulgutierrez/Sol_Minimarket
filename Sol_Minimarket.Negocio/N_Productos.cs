c0411d9e1f6f5787060eea3580048254ff43031b
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2024-04-06 20:54:42 -0600

    Capa de negocio de productos

---
+++ Sol_Minimarket.Negocio/N_Productos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Sol_Minimarket.Entidades;
+using Sol_Minimarket.Datos;
+
+namespace Sol_Minimarket.Negocio
+{
+    public class N_Productos
+    {
+        public static DataTable Listado_pr(string cTexto)
+        {
+            D_Productos Datos = new D_Productos();
+            return Datos.Listado_pr(cTexto);
+        }
+
+        // Comunicación entre la capa de interfaz con la capa de Datos
+        public static string Guardar_pr(int nOpcion, E_Productos oPr)
+        {
+            D_Productos Datos = new D_Productos();
+            return Datos.Guardar_pr(nOpcion, oPr);
+        }
+
+        // Comunicación entre la capa de interfaz con la capa de Datos
+        public static string Eliminar_pr(int Codigo_pr)
+        {
+            D_Productos Datos = new D_Productos();
+            return Datos.Eliminar_pr(Codigo_pr);
+        }
+    }
+}
